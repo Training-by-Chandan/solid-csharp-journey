d03aff68f7e44f6cfaed53063fcfab3e506612d7
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-05-17 17:03:40 +0545

    console project added

---
+++ ConsoleApp/Solid.CSharp/Solid.CSharp.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Solid.CSharp.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //printing hello world
+            Console.WriteLine("Hello World ");
+        }
+    }
+}
