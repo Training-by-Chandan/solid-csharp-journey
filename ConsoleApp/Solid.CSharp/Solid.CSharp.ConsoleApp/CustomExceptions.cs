721f2bc90a1658976113611aadad62b2299658dc
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-06-12 17:38:02 +0545

    exception handling

---
+++ ConsoleApp/Solid.CSharp/Solid.CSharp.ConsoleApp/CustomExceptions.cs
@@ -0,0 +1,46 @@
+﻿namespace Solid.CSharp.ConsoleApp
+{
+    [Serializable]
+    public class TestException : Exception
+    {
+        public TestException()
+        { }
+
+        public TestException(string message) : base(message)
+        {
+        }
+
+        public TestException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        public void WriteInFile()
+        {
+            //code to write the exceptions in file
+        }
+    }
+
+    [Serializable]
+    public class NUmber10Exception : Exception
+    {
+        public NUmber10Exception()
+        {
+            //WriteInFile();
+        }
+
+        public NUmber10Exception(string message) : base(message)
+        {
+           // WriteInFile();
+        }
+
+        public NUmber10Exception(string message, Exception inner) : base(message, inner)
+        {
+            //WriteInFile();
+        }
+
+        public void WriteInFile()
+        {
+            //code to write the exceptions in file
+        }
+    }
+}
